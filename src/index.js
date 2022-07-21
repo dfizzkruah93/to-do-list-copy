08b3c7a8a13c8e889cfaa710a96c09042f9ef06a
George D. Kruah <dfizzkruah@gmail.com>
2022-07-21 10:26:34 +0300

    Add webpack files

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './index.scss';
+import Task from './task.js';
+import Todos from './todos.js';
+import Store from './store.js';
+
+// NOTE: Tasks are only Edited on Enter Key Press
+const form = document.getElementById('todo-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (form.elements.todo.value) {
+    const todos = Store.getTodos();
+    const input = form.elements.todo.value;
+    const task = new Task(todos.length + 1, input);
+    Store.add(task);
+    Todos.load();
+    form.reset();
+  }
+});
+
+// Dynamically render todos on page load
+Todos.load();
+
+// Clear all Checked Todos whwn clicked
+const clearBtn = document.querySelector('section > button');
+clearBtn.addEventListener('click', () => {
+  const todos = document.querySelectorAll('.task');
+  let todosStore = Store.getTodos();
+  todos.forEach((todo) => {
+    if (todo.firstChild.checked) todo.remove();
+  });
+  todosStore = todosStore.filter((todo) => !todo.completed);
+  todosStore.forEach((todo, index) => {
+    todo.index = index + 1;
+  });
+  Store.setTodos(todosStore);
+});
+
+Todos.editMode();
